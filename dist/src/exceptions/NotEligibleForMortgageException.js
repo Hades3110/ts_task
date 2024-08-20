cfa4ba86a87ec7a15e75b6d22b44b4957201e801
Narek Galstyan <narekgalstyanh@gmail.com>
2024-08-20 11:25:00 +0400

    Remove dist folder

--- dist/src/exceptions/NotEligibleForMortgageException.js
+++
@@ -1,7 +0,0 @@
-export default class NotEligibleForMortgageException extends Error {
-    constructor(message) {
-        super(message);
-        this.name = 'NotEligibleForMortgageException';
-    }
-}
-//# sourceMappingURL=NotEligibleForMortgageException.js.map
--- dist/src/exceptions/WrongDataException.js
+++
@@ -1,7 +0,0 @@
-export default class WrongDataException extends Error {
-    constructor(message) {
-        super(message);
-        this.name = 'WrongDataException';
-    }
-}
-//# sourceMappingURL=WrongDataException.js.map
