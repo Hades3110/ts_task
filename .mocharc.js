ffde3704c2799057b115c0ca7c06d98e4ae82fa1
AbAyvazyan <admin@MacBookAir.local>
2024-08-21 14:24:18 +0400

    Feat:New Homework

---
+++ .mocharc.js
@@ -0,0 +1,6 @@
+module.exports = {
+    require: 'ts-node/register',
+    spec: '**/*.spec.ts',
+    extension: ['ts'],
+    timeout: 5000,
+};
---
+++ src/MortgageApplicationQueueProcessor.ts
@@ -0,0 +1,42 @@
+import { WrongDataException } from './exceptions/WrongDataException';
+
+interface Customer {
+    updateBalance: (amount: number) => void;
+}
+
+interface CustomerRepository {
+    get: (id: string) => Customer | null;
+}
+
+class MortgageApplicationQueueProcessor {
+    private customerRepository: CustomerRepository;
+
+    constructor(customerRepository: CustomerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    static MESSAGE_INVALID_CUSTOMER = 'Customer not found!';
+
+    private checkWrongData(customer: Customer | null): void {
+        if (!customer) {
+            throw new WrongDataException(MortgageApplicationQueueProcessor.MESSAGE_INVALID_CUSTOMER);
+        }
+    }
+
+    public processRequest(customerId: string | number, amountRequested: string | number): void {
+        this.updateBalance(String(customerId), +amountRequested);
+    }
+
+    private updateBalance(customerId: string, amountRequested: number): void {
+        const customer = this.getCustomer(customerId);
+        customer.updateBalance(amountRequested);
+    }
+
+    private getCustomer(customerId: string): Customer {
+        const customer = this.customerRepository.get(customerId);
+        this.checkWrongData(customer);
+        return customer as Customer;
+    }
+}
+
+export { MortgageApplicationQueueProcessor };
